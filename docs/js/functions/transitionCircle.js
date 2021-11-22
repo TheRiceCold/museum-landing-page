cde763803b5c4d92428ec4df1b06bd236dd89461
Walter <dalewaltergh@gmail.com>
2021-11-23 02:47:26 +0800

    fixed mozila double scroll

--- docs/js/functions/transitionCircle.js
+++ docs/js/functions/transitionCircle.js
@@ -1,22 +1,20 @@
-
 export function transitionCircle() {
+  const homeLink = document.getElementById('home-arc-link')
   const eventLink = document.getElementById('events-page-link')
-  const eventLink = document.getElementById('gallery-page-link')
-  const eventLink = document.getElementById('places-page-link')
-
+  const galleryLink = document.getElementById('gallery-page-link')
+  const placesLink = document.getElementById('places-page-link')
   eventLink.addEventListener('click', () => clickedLink('event-page'))
   
-  document.getElementById('home-arc-link')
-    .addEventListener('click', () => {
-      document.querySelector('.transition-circle#home-arc').classList.add('active')
-      document.querySelector('.transition-circle#event-page').classList.remove('active')
-      setTimeout(() => document.body.style.overflow = '', 450)
-    })
+  homeLink.addEventListener('click', () => {
+    document.querySelector('.transition-circle#home-arc').classList.add('active')
+    document.querySelector('.transition-circle#event-page').classList.remove('active')
+    setTimeout(() => document.body.style.overflow = '', 450)
+  })
 }
 
 function clickedLink(id) {
   let homeLink = document.querySelector('.transition-circle#home-arc')
   document.querySelector('.transition-circle#'+id).classList.add('active')
   homeLink.classList.remove('active')
-  document.body.style.overflow = 'hidden'
+  setTimeout(() => document.body.style.overflow = 'hidden', 300)
 }
