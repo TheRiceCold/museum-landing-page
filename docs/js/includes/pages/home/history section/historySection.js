f039a3d00d7d1ccec80cceaedcf0de728eca7ead
Walter <dalewaltergh@gmail.com>
2021-11-24 14:18:52 +0800

    fixing media query and cleaning up the code

--- docs/js/includes/pages/home/history section/historySection.js
+++ docs/js/includes/pages/home/history section/historySection.js
@@ -1,7 +1,7 @@
 const imgSrc = 'img/history-image.png'
 const description = 'Lorem ipsum dolor consectetur adipisicing elit. Dolores odio voluptates quae veritatis a nihil molestiae quibusdam voluptatibus, eveniet recusandae mollitia maiores incidunt ipsum in aperiam. A cum fugiat corporis ratione dicta fugit, laborum harum, minima obcaecati quod facilis? Sint eius similique exercitationem tempora inventore libero possimus, veritatis reiciendis!'
 const title = 'History'
-const btnLabel = 'Show Me More'
+const btnLabel = 'more history'
 
 export let historySection = `
 <section id="history-arc">
