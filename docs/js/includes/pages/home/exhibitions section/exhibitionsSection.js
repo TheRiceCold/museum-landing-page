414efcf38456a1ea0530ec248192a3677e077097
Walter <dalewaltergh@gmail.com>
2021-11-25 12:46:59 +0800

    testing JSON

--- docs/js/includes/pages/home/exhibitions section/exhibitionsSection.js
+++ docs/js/includes/pages/home/exhibitions section/exhibitionsSection.js
@@ -1,5 +1,14 @@
 import { card } from './exhibitionsCards.js'
-import { cards } from './cards.js'
+import cards from './cards.json' assert { type: 'json'}
+
+function loadData(data) {
+  return fetch(`./${data}.json`)
+    .then(r => r.json())
+      .then(d => console.log(d))
+} 
+
+loadData(cards)
+// loadData('cards').then(d => console.log(d))
 
 let getCards = cards.map(c => card(c.month, c.day, c.year, c.artist, c.title, c.description, c.btnLabel)).join('')
 
