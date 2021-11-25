6bda49d2305ecfd3ab19aa89e3721c674915d9bb
Walter <dalewaltergh@gmail.com>
2021-11-25 15:22:13 +0800

    done media query on small devices

--- docs/js/includes/pages/home/exhibitions section/exhibitionsSection.js
+++ docs/js/includes/pages/home/exhibitions section/exhibitionsSection.js
@@ -1,20 +1,20 @@
 import { card } from './exhibitionsCards.js'
-// import cards from './cards.json' assert { type: 'json'}
+import cards from './cards.json' assert { type: 'json'}
 
-function loadData(data) {
-  return fetch(`./${data}.json`)
-    .then(r => r.json())
-      .then(d => console.log(d))
-} 
+// function loadData(data) {
+//   return fetch(`./${data}.json`)
+//     .then(r => r.json())
+// } 
 
-loadData('cards')
+// console.log(loadData('cards'))
 
-// let getCards = cards.map(c => card(c.month, c.day, c.year, c.artist, c.title, c.description, c.btnLabel)).join('')
+let getCards = cards.map(c => card(c.month, c.day, c.year, c.artist, c.title, c.description, c.btnLabel)).join('')
 
 export const exhibitionsSection = `
 <section id="exhibition-arc">
   <h1 id="heading">exhibitions</h1>
   <div class="row">
+    ${getCards}
   </div>
 </section>
 `
