22e62e1bc2b661639563209a0d55693e055f8b35
Walter <dalewaltergh@gmail.com>
2021-11-25 13:20:32 +0800

    test json again

--- docs/js/includes/pages/home/exhibitions section/exhibitionsSection.js
+++ docs/js/includes/pages/home/exhibitions section/exhibitionsSection.js
@@ -7,15 +7,14 @@ function loadData(data) {
       .then(d => console.log(d))
 } 
 
-loadData(cards)
+loadData('cards')
 
-let getCards = cards.map(c => card(c.month, c.day, c.year, c.artist, c.title, c.description, c.btnLabel)).join('')
+// let getCards = cards.map(c => card(c.month, c.day, c.year, c.artist, c.title, c.description, c.btnLabel)).join('')
 
 export const exhibitionsSection = `
 <section id="exhibition-arc">
   <h1 id="heading">exhibitions</h1>
   <div class="row">
-  ${getCards}
   </div>
 </section>
 `
