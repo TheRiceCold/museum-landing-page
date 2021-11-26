8eddf458e3f091c8533a47a627bba9167818496e
Walter <dalewaltergh@gmail.com>
2021-11-26 14:04:04 +0800

    home page media query

--- docs/js/includes/pages/home/exhibitions section/exhibitionsSection.js
+++ docs/js/includes/pages/home/exhibitions section/exhibitionsSection.js
@@ -9,6 +9,15 @@ import {cards} from './cards.js'
 
 // console.log(loadCards('cards'))
 
+let request = new XMLHttpRequest()
+const url = 'https://raw.githubusercontent.com/Dalewaltergh/Activity5-product-landing-page/main/docs/json/cards.json'
+request.open('GET', url) 
+request.onload = () => {
+  let data = JSON.parse(request.responseText)
+  console.log(data)
+}
+request.send()
+
 let getCards = cards.map(c => card(c.month, c.day, c.year, c.artist, c.title, c.description, c.btnLabel)).join('')
 
 export const exhibitionsSection = `
